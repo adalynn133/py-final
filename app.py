84c253691c83d16ac7daf8d43c33294260beee7f
adalynn133 <169125708+adalynn133@users.noreply.github.com>
2024-05-25 22:42:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def handle_location(event):
     try:
         for place in places_result['results']:
             location_message = LocationSendMessage(
-                title=place['plus_code'],
+                title=place['geometry'],
                 address=place.get('vicinity', 'No address provided'),
                 latitude=place['geometry']['location']['lat'],
                 longitude=place['geometry']['location']['lng']
