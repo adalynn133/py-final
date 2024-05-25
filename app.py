b848885f1ae4c2b51711d55ab7de9b26fd993c40
adalynn133 <169125708+adalynn133@users.noreply.github.com>
2024-05-25 16:53:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,12 +44,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    if msg == "停車場" or msg == "加油站":
-        flex_message = TextSendMessage(text='請導入您的位置',
-                               quick_reply=QuickReply(items=[
-                                   QuickReplyButton(action=LocationAction(label="位置"))
+    flex_message = TextSendMessage(text='請導入您的位置',
+                            quick_reply=QuickReply(items=[
+                                QuickReplyButton(action=LocationAction(label="位置"))
                                ]))
-        line_bot_api.reply_message(event.reply_token, flex_message)
+    line_bot_api.reply_message(event.reply_token, flex_message)
        
     
         
