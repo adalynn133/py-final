69b7c24848ad5678190f2b0cd16e364edcb032f0
adalynn133 <169125708+adalynn133@users.noreply.github.com>
2024-05-25 18:25:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def handle_location(event):
     for place in places_result['results']:
         location_message = LocationSendMessage(
             title= place['name'],
-            latitude=place['lat']
+            latitude=place['lat'],
             longitude=['lng']
         )
         line_bot_api.reply_message(event.reply_token, location_message)
